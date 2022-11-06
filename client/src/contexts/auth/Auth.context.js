1d5efa36216f37b9343e39f7236fb3fba6b986e1
s2wksakaqh <s2wksakaqh@gmail.com>
2022-11-06 20:55:26 +0900

    commit

--- client/src/contexts/auth/Auth.context.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-export const AuthenticationContext = createContext();
-
-export default AuthenticationContext;
--- client/src/contexts/types.js
+++
@@ -1,4 +0,0 @@
-export const FETCH_USER_DATA = "FETCH_USER_DATA";
-export const UPDATE_FOLLOW_DATA = "UPDATE_FOLLOW_DATA";
-export const BOOKMARK_POST = "BOOKMARK_POST";
-export const LOGOUT = "LOGOUT";
