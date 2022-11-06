8da2f82587ff509cf5899bd18041bda44bb2471d
s2wksakaqh <s2wksakaqh@gmail.com>
2022-11-06 20:13:11 +0900

    init

--- client/src/utils/regex.js
+++
@@ -1,5 +0,0 @@
-// the Regex email validation was token from : https://emailregex.com/
-
-export const EmailRegex =
-	// eslint-disable-next-line no-useless-escape
-	/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
--- server/helpers/savePhoto.js
+++
@@ -1,8 +0,0 @@
-const savePhoto = (post, photoEncoded, photoType) => {
-	if (photoEncoded != null) {
-		post.Photo = new Buffer.from(photoEncoded, "base64");
-		post.PhotoType = photoType;
-	}
-};
-
-module.exports = { savePhoto };
